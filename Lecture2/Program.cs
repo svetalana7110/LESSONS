dbdb085f0a52d98b56fba4e8634c04614a8d4738
svetalana7110 <svetlanapab@gmail.com>
2023-03-30 18:48:14 +0300

    Решила первую задачу

--- Lecture2/Program.cs
+++ Lecture2/Program.cs
@@ -37,7 +37,6 @@ if (c3 > max){
      max = c3;
 }
 Console.WriteLine(max);
-*/
 
 int Max(int arg1, int arg2, int arg3)
 {
@@ -67,3 +66,88 @@ Max(a2, b2, c2),
 Max(a3, b3, c3));
 
 Console.WriteLine(max);
+
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result=arg2;
+    if(arg3>result) result=arg3;
+    return result;
+}
+int[] array = {55,66,77,888,99,345,66,44,33};
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+    );
+Console.WriteLine(result);
+
+int[] array = {1,12,31,4,15,15,17,18};
+int n = array.Length;
+int find = 15;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
+*/
+void FillArray(int[] collection)
+{
+     int Length = collection.Length;
+     int index = 0;
+     while (index < Length)
+     {
+          collection[index] = new Random().Next(1, 10);
+          index++;
+     }
+
+}
+
+void PrintArray(int[] col)
+{
+     int count = col.Length;
+     int position = 0;
+     while (position < count)
+     {
+          Console.WriteLine(col[position]);
+          position++;
+     }
+}
+
+int IndexOf(int[]collection,int find)
+
+{
+     int count = collection.Length;
+     int index = 0;
+     int position = -1;
+     while (index < count)
+     {
+          if(collection[index] == find)
+          {
+               position = index;
+               break;
+          }
+          index++;
+     }
+     return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 9;
+array[8] = 2;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array,9);
+Console.WriteLine(pos);
--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -42,7 +42,7 @@ bool bl = false;
 string tmp = $"{y} - лалалал {str} fldsfdsfsdf {ch} - sdflnsdf s{db} {bl}";
 string tmp2 = y + " - лалалал " + str + " fldsfdsfsdf " + " - sdflnsdf s";
 Console.WriteLine(tmp);
-*/
+
 // Напишите программу, которая выводит случайное число из отрезка [10, 99] 
 // и показывает наибольшую цифру числа.
 // Например:
@@ -64,4 +64,32 @@ else{
     Console.WriteLine(firstDigit);
 }
 int b = 5;
+//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру 
+этого числа.
+● 456 -> 46
+● 782 -> 72
+● 918 -> 98
 
+Random random = new Random();
+int randomValue = random.Next( 100, 1000);
+int firstDigit = randomValue / 100 ;
+int secondDigit = randomValue % 10;
+Console.WriteLine(randomValue);
+Console.WriteLine($"{firstDigit} {secondDigit}");
+
+Random random = new Random();
+int randomValue = random.Next( 100, 1000);
+int firstDigit = randomValue / 100 * 10;
+int secondDigit = randomValue % 10;
+Console.WriteLine(randomValue);
+
+Console.WriteLine(firstDigit + secondDigit);
+
+//Напишите программу, которая будет принимать на вход два числа и выводить, является ли
+второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток 
+от деления.
+● 34, 5 -> не кратно, остаток 4
+● 16, 4 -> кратно
+*/
+int c = 4;
+int g = 3;
