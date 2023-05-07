7e406df57c82ad99b989c48c5edbbc6011457f61
svetalana7110 <svetlanapab@gmail.com>
2023-05-07 15:30:41 +0300

    "Добавила решение первой задачи"

--- WORK9/Program.cs
+++ WORK9/Program.cs
@@ -2,7 +2,7 @@
 все натуральные числа в промежутке от N до 1. Выполнить с 
 помощью рекурсии.
 N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 Print(1, 9);
 void Print(int number, int i)
 {
@@ -11,6 +11,29 @@ void Print(int number, int i)
         Console.Write($"{i} ");
         Print(number, i - 1);
     }
+}
+Решение 2*/
+int n = InputInt("Введите положительное число");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
 }
 /* Задача 66.Задайте значения M и N. Напишите программу, которая найдёт 
 сумму натуральных элементов в промежутке от M до N.Выполнить с 
